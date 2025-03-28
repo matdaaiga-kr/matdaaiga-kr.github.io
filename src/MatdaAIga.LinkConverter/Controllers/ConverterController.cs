38eca73aff392fd377430526dfc9387db088a25d
Kim AeYong <79782610+YoonKeumJae@users.noreply.github.com>
2025-03-28 16:51:58 +0900

    ConverterController.RunAsync() 구현 (#48)
    * yaml schema configuration
    
    * Converter Controller
    
    * annotation add
    
    * fix annotation
    
    * fix annotation
    
    * fix typo: arrange help messages
    
    * ADD: argument options test code
    
    * ADD: RunAsync test
    
    * fix test logic
    
    * fix test code
    
    * fix arg options
    
    * fix test code
    
    * code & config file fix
    
    * code & config file fix

---
+++ src/MatdaAIga.LinkConverter/Controllers/ConverterController.cs
@@ -0,0 +1,37 @@
+using MatdaAIga.LinkConverter.Services;
+using MatdaAIga.LinkConverter.Options;
+
+namespace MatdaAIga.LinkConverter.Controllers;
+
+/// <summary>
+/// This represents the controller entity that invokes the conversion workflow
+/// </summary>
+public class ConverterController(IConverterService service): IConverterController
+{
+    private readonly IConverterService _service = service ?? throw new ArgumentNullException(nameof(service));
+
+    /// <inheritdoc />
+    public async Task RunAsync(string[] args)
+    {
+        var options = ArgumentOptions.Parse(args);
+        if (options.Help)
+        {
+            this.DisplayHelp();
+            return;
+        }
+
+        var data = await this._service.LoadAsync(options.Filepath).ConfigureAwait(false);
+        var markdown = await this._service.ConvertAsync(data).ConfigureAwait(false);
+        await this._service.SaveAsync(markdown, options.Filepath).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Displays help
+    /// </summary>
+    private void DisplayHelp()
+    {
+        Console.WriteLine("Usage >>");
+        Console.WriteLine("  -f | --filepath    Specify the absolute filepath of a YAML file to convert");
+        Console.WriteLine("  -h | --help        Display help");
+    }
+}
---
+++ tests/MatdaAIga.LinkConverter.Tests/RunAsyncTests.cs
@@ -0,0 +1,73 @@
+using MatdaAIga.LinkConverter.Controllers;
+using MatdaAIga.LinkConverter.Services;
+using MatdaAIga.LinkConverter.Models;
+using NSubstitute;
+using Shouldly;
+
+namespace MatdaAIga.LinkConverter.Tests;
+
+/// <summary>
+/// This represents test entity for the <see cref="ConverterController" /> class.
+/// </summary>
+public class RunAsyncTests
+{
+    // TODO: 파일 입출력으로 실제 동작 여부 테스트, Service 구현 후 진행
+    // /// <summary>
+    // /// Tests the RunAsync method.
+    // /// </summary>
+    // [Fact]
+    // public async Task RunAsyncTest()
+    // {
+    //     // Arrange
+    //     var tempFilePath = Path.GetTempFileName(); 
+    //     await File.WriteAllTextAsync(tempFilePath, 
+    //     """
+    //     - name: Test
+    //       links:
+    //         - name: Test Link
+    //           url: https://test.com
+    //     """
+    //     ); 
+
+    //     var args = new[] { "-f", tempFilePath };
+    //     var service = Substitute.For<IConverterService>();
+    //     var controller = new ConverterController(service);
+
+    //     var linkCollection = new LinkCollection 
+    //     { 
+    //         Name = "Test", 
+    //         Links = new List<LinkItem>
+    //         {
+    //             new LinkItem { Title = "Test Link", Url = "https://test.com" }
+    //         } 
+    //     };
+    //     var markdown = 
+    //     """
+    //     Title: Test
+    //     ---
+    //     - [Test Link](https://test.com)
+    //     ---
+    //     """;
+        // service.LoadAsync(tempFilePath).Returns(Task.FromResult(linkCollection));
+        // service.ConvertAsync(linkCollection).Returns(Task.FromResult(markdown));
+        // service.SaveAsync(markdown, tempFilePath).Returns(Task.CompletedTask);
+
+        // try
+        // {
+        //     // Act
+        //     await controller.RunAsync(args);
+
+        //     // Assert 
+        //     (await service.Received(1).LoadAsync(tempFilePath)).ShouldBe(linkCollection);
+        //     (await service.Received(1).ConvertAsync(linkCollection)).ShouldBe(markdown);
+        //     await service.Received(1).SaveAsync(markdown, tempFilePath);
+        // }
+        // finally
+        // {
+        //     if (File.Exists(tempFilePath))
+        //     {
+        //         File.Delete(tempFilePath);
+        //     }
+        // }
+    // }
+}
