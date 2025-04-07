703ad43dc79ba53128bd3fad904c33e73700b2bd
YoonKeumJae <ykj1512@gmail.com>
2025-04-07 13:52:50 +0900

    connect Program.cs and Controller

--- src/MatdaAIga.LinkConverter/Program.cs
+++ src/MatdaAIga.LinkConverter/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MatdaAIga.LinkConverter.Controllers;
+using MatdaAIga.LinkConverter.Services;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var controller = new ConverterController(new ConverterService());
+        await controller.RunAsync(args);
+    }
+}
