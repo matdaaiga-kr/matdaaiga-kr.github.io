4d2ab739e9b970ac63dae146980ec29d3fb67e46
YoonKeumJae <ykj1512@gmail.com>
2025-04-09 13:31:52 +0900

    fix: builder pattern

--- src/MatdaAIga.LinkConverter/Program.cs
+++ src/MatdaAIga.LinkConverter/Program.cs
@@ -4,27 +4,14 @@ using MatdaAIga.LinkConverter.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-/// <summary>
-/// The entry point for the application
-/// </summary>
-public class Program
-{
-    /// <summary>
-    /// The main entry point for the application
-    /// </summary>
-    /// <param name="args">The command-line arguments</param>
-    public static async Task Main(string[] args)
+var host = Host.CreateDefaultBuilder(args)
+    .UseConsoleLifetime()
+    .ConfigureServices((context, services) =>
     {
-        var host = Host.CreateDefaultBuilder(args)
-            .UseConsoleLifetime()
-            .ConfigureServices((context, services) =>
-            {
-                services.AddScoped<IConverterService, ConverterService>();
-                services.AddScoped<ConverterController>();
-            })
-            .Build();
+        services.AddScoped<IConverterService, ConverterService>();
+        services.AddScoped<ConverterController>();
+    })
+    .Build();
 
-        var controller = host.Services.GetRequiredService<ConverterController>();
-        await controller.RunAsync(args);
-    }
-}
+var controller = host.Services.GetRequiredService<ConverterController>();
+await controller.RunAsync(args);
